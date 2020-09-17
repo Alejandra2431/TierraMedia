3721e2cdbe5400cf9f3199e7ffbe542dfaa080c2
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-16 22:48:04 -0300

    asDASD

--- src/Library/Humano.cs
+++ src/Library/Humano.cs
@@ -1,7 +1,7 @@
 using System;
 using Elemento;
 
-namespace Personajes
+namespace Personaje
 {
     public class Humano
     {
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,7 +8,17 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            
+            Arco arco1= new Arco("Arco Fuego",20,0);
+            Anillo anillo1 =new Anillo("Anillo Brillante",10,20);
+            Elfo elfo1= new Elfo("Legolas",arco1,anillo1);
+
+            Mazo mazo1= new Mazo("Martillo",40,0);
+            Armadura armadura1=new Armadura("Armadura Hierro",0,50);
+            Enano enano1=new Enano("Gimbli",mazo1,armadura1);
+
+            Espada espada1=new Espada("Espada Acero",50,5);
+            Escudo escudo1= new Escudo("Escudo Madera",0,40);
+            Humano humano1=new Humano("Charlie",espada1,escudo1);
         }
     }
 }
